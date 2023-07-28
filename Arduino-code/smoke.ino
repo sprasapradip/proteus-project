4a407c996d3e20e34b392cb47aca421d2bd5ed07
Pradip Subedi <114067364+sprasapradip@users.noreply.github.com>
2023-07-28 07:59:01 +0545

    Update smoke.ino
    Signed-off-by: Pradip Subedi <114067364+sprasapradip@users.noreply.github.com>

--- Arduino-code/smoke.ino
+++ Arduino-code/smoke.ino
@@ -1,1 +1,16 @@
 
+void setup() {
+  // Initialize serial communication for debugging (optional)
+  Serial.begin(9600);
+  // Set the gas sensor pin as an input
+  pinMode(gasSensorPin, INPUT);
+}
+
+void loop() {
+  // Read the analog value from the gas sensor
+  int gasValue = analogRead(gasSensorPin);
+
+  // Print the raw analog value (optional, for debugging)
+  Serial.print("Gas Value: ");
+  Serial.println(gasValue);
+
