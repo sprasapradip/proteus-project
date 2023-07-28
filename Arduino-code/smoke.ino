d8ce314b5ae94dd8bad4a6d7e2210d9a1163c2c4
Pradip Subedi <114067364+sprasapradip@users.noreply.github.com>
2023-07-28 07:59:48 +0545

    Update smoke.ino
    Signed-off-by: Pradip Subedi <114067364+sprasapradip@users.noreply.github.com>

--- Arduino-code/smoke.ino
+++ Arduino-code/smoke.ino
@@ -1,3 +1,8 @@
+// Define the analog pin for the gas/smoke sensor
+const int gasSensorPin = A0;
+
+// Threshold value to trigger an alarm. Adjust this according to your sensor's sensitivity
+const int gasThreshold = 500; // Change this value based on your sensor's calibration
 
 void setup() {
   // Initialize serial communication for debugging (optional)
